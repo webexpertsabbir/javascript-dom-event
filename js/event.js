a64943b7eec29d8349aae6670e55772adf07b2d7
Sabbir <webexpertsabbir@gmail.com>
2022-08-14 01:21:36 +0600

    onclick final use

--- js/event.js
+++ js/event.js
@@ -18,3 +18,25 @@ const makePurpleButton = document.getElementById('make-purple');
 makePurpleButton.onclick = function makePurple(){
     document.body.style.backgroundColor = 'purple';
 }
+
+// option 4
+
+const buttonPink = document.getElementById('make-pink');
+buttonPink.addEventListener('click', makePink);
+
+function makePink(){
+    document.body.style.backgroundColor = 'pink';
+}
+
+// option 4 another
+
+const greenButton = document.getElementById('make-green');
+greenButton.addEventListener('click', function makeGreen(){
+    document.body.style.backgroundColor = 'green';
+});
+
+// option 4  final use this sometime
+ 
+document.getElementById('make-goldenrod').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'goldenrod';
+});
